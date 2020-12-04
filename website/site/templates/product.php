4771138631551e031e7409c861df54aca320f260
Christopher Supnig <christopher@supnig.com>
2020-12-04 22:11:09 +0100

    product

--- website/site/templates/product.php
+++ website/site/templates/product.php
@@ -1,20 +1,25 @@
-<?php snippet('header');
+<?php snippet('head');
 //Set background image
 if ($page->backgroundImage()->isNotEmpty()) $backgroundStyle = "background-image:url(".$page->backgroundImage()->url().")"; 
 else $backgroundStyle = ""; ?>
 <body class="product beigeBackground" style="<?= $backgroundStyle ?>">
-	<?php snippet('head'); ?>
-  <section class="product width100 padding">
+	<?php snippet('header'); ?>
+  <section class="product width100 largeTopPadding padding">
     <div class="width2C topMargin">
 
         <div class="item">
           <div class="width100 square">
-            <?php if ($image = $page->image()->toFile()) { ?>
+            <?php if ($image = $page->productImage()->toFile()) { ?>
               <img class="width100 cover" src="<?= $image->url() ?>" alt="<?= $page->name()->html() ?>"/>
             <?php } ?>
-            <div class="overlay darkGreenCutOut relative">
-              <span class="footnote top right"><?= $page->category()->html() ?></span>
-            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="width100">
+            <h1 class="small darkGreen"><?= $page->name()->kirbyText() ?></h1>
+            <p class="darkGreen large">
+              <?= $page->description()->kirbyText() ?>
+            </p>
           </div>
         </div>
 
