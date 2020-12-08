c99e3804d8c8302e7b4a1d85f9992ca2e7daef68
Christopher Supnig <christopher@supnig.com>
2020-12-08 15:47:26 +0100

    navi-draft

--- website/site/snippets/header.php
+++ website/site/snippets/header.php
@@ -1,24 +1,26 @@
 <?php $headerColor = "darkGreen";
 $headerBackgroundColor = "whiteBackground"; ?>
 
-<header class="width100 absolute padding">
-  <nav class="collapsed width100 <?= $headerColor ?> noUnderline">
-    <div class="width25 logo floatLeft">
+<header class="width100 noUnderline noWrap">
+  <nav class=" width100 <?= $headerColor ?>">
+    <div class="width25 logo absolute">
       <a href="<?=$site->url() ?>"><img class="width100" src="<?= $site->url() ?>/../assets/images/logo_<?= $headerColor ?>.png" alt="<?= $site->name()->html() ?>"/></a>
     </div>
-    <div class="navigation floatRight">
-      <?php foreach($site->children()->listed() as $item) { ?>
-          <div class="primary smallLeftMargin floatLeft relative darkGreen <?= $item->title()->html() ?> <?= r($item->isOpen(), 'active strong bottomBorder') ?>">
-            <a href="<?= $item->url() ?>" title="<?= $item->name()->html() ?>"><?= $item->name()->html() ?></a>
-            <div class="secondary absolute noWrap">
-              <?php foreach ($item->children()->listed() as $subitem) { ?>
-                <a href="<?= $subitem->url() ?>" title="<?= $subitem->name()->html() ?>"><?= $subitem->name()->html() ?></a><br/>
-              <?php } ?>
-            </div>
-          </div>
-      <?php } ?>
+    <div class="navigation">
+      <ul class="padding">
+        <?php foreach($site->children()->listed() as $item) { ?>
+            <li class="darkGreen <?= $item->title()->html() ?> <?= r($item->isOpen(), 'active strong bottomBorder') ?>">
+              <a href="<?= $item->url() ?>" title="<?= $item->name()->html() ?>"><?= $item->name()->html() ?></a>
+              <ul class="">
+                <?php foreach ($item->children()->listed() as $subitem) { ?>
+                  <li>
+                    <a href="<?= $subitem->url() ?>" title="<?= $subitem->name()->html() ?>"><?= $subitem->name()->html() ?></a>
+                  </li>
+                <?php } ?>
+              </ul>
+            </li>
+        <?php } ?>
+      </ul>
     </div>
   </nav>
-  <div class="toggle"></div>
-  <div class="close"></div>
 </header>
